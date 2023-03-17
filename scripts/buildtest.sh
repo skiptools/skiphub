6a4ea8b34bf587b992f40d75226a023e94f1bacb
Marc Prud'hommeaux <marc@prux.org>
2023-03-17 10:13:23 -0400

    CI

--- scripts/buildtest.sh
+++ scripts/buildtest.sh
@@ -11,16 +11,13 @@ for TARGET in \
     # start with a clean DD
     #rm -rf ~/Library/Developer/Xcode/DerivedData
 
-    xcodebuild test -skipPackagePluginValidation -configuration Debug -sdk "macosx" -destination "platform=macosx" -scheme ${TARGET}Kotlin | tee xcodebuild.log | xcpretty --report junit
-
-    tree ~/Library/Developer/Xcode/DerivedData
-
-    date
+    xcodebuild test -skipPackagePluginValidation -configuration Debug -sdk "macosx" -destination "platform=macosx" -scheme ${TARGET}Kotlin | tee xcodebuild.log
+    # | xcpretty --report junit
+    # -only-testing:${TARGET}Tests 
 
     # get the most recent derived data folder
     DER=`ls -1rtd ~/Library/Developer/Xcode/DerivedData/skip-core-*/SourcePackages/plugins/skip-core.output | tail -n 1`
 
-    # -only-testing:${TARGET}Tests 
     tree -lh -I build ${DER}/${TARGET}KotlinTests/SkipTranspilePlugIn/
 
     # run the tests for the target
