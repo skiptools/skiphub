7d3e027706bbb5f3f461b4b880772ac6a2703121
Marc Prud'hommeaux <marc@prux.org>
2023-03-17 12:03:36 -0400

    CI

--- scripts/buildtest.sh
+++ scripts/buildtest.sh
@@ -1,20 +1,17 @@
 #!/bin/sh -ve
 set -o pipefail
 
+# example usage: ./scripts/buildtest.sh Debug SkipLib
 CONFIG=${1:="Debug"}
+TARGETS=${2:="SkipLib"}
+#TARGETS=${2:="SkipLib SkipFoundation SkipUI ExampleLib ExampleApp"}
 
-for TARGET in \
-    SkipLib \
-    SkipFoundation \
-    SkipUI \
-    ExampleLib \
-    ExampleApp \
-    ; do
+for TARGET in ${TARGETS}; do
     # start with a clean DD
     #rm -rf ~/Library/Developer/Xcode/DerivedData
 
     # make sure the non-Kotlin tests pass on iOS
-    xcodebuild test -skipPackagePluginValidation -configuration "${CONFIG}" -sdk "iphonesimulator" -destination "platform=iOS Simulator,name=iPhone 14 Pro" -scheme ${TARGET} | tee xcodebuild-ios-${TARGET}.log
+    xcodebuild test -skipPackagePluginValidation -configuration "${CONFIG}" -sdk "iphonesimulator" -destination "platform=iOS Simulator,name=iPhone SE (3rd generation)" -scheme ${TARGET} | tee xcodebuild-ios-${TARGET}.log
 
     xcodebuild test -skipPackagePluginValidation -configuration "${CONFIG}" -sdk "macosx" -destination "platform=macosx" -scheme ${TARGET}Kotlin | tee xcodebuild-macos-${TARGET}.log
     # | xcpretty --report junit
