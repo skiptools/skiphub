4c6bad23da759eff1c2ab53c59969d762bee9aca
marcprux <mwp1@cornell.edu>
2023-04-03 14:01:42 -0400

    Add target version checks

--- Sources/SkipUI/SkipUI.swift
+++ Sources/SkipUI/SkipUI.swift
@@ -7,39 +7,67 @@
 @_exported import SkipFoundation
 import SwiftUI
 
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
 public typealias App = SwiftUI.App
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
 public typealias Scene = SwiftUI.Scene
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
 public typealias WindowGroup = SwiftUI.WindowGroup
 
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
 public typealias View = SwiftUI.View
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
 public typealias State = SwiftUI.State
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
 public typealias Binding = SwiftUI.Binding
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
 public typealias ObservableObject = SwiftUI.ObservableObject
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
 public typealias EnvironmentObject = SwiftUI.EnvironmentObject
 
+//@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
 //public typealias ForEach = SwiftUI.ForEach
 
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
 public typealias HStack = SwiftUI.HStack
+
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
 public typealias VStack = SwiftUI.VStack
 
+//@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
 //public typealias Rectangle = SwiftUI.Rectangle
 
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
 public typealias Text = SwiftUI.Text
+
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
 public typealias Label = SwiftUI.Label
+
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
 public typealias Color = SwiftUI.Color
+
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
 public typealias Button = SwiftUI.Button
+
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
 public typealias TabView = SwiftUI.TabView
+
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
 public typealias TextField = SwiftUI.TextField
+
+
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+public typealias Published = SwiftUI.Published
+
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+public typealias StateObject = SwiftUI.StateObject
+
 
 @available(macOS 13, iOS 16, tvOS 16, watchOS 8, *)
 public typealias NavigationLink = SwiftUI.NavigationLink
 
 @available(macOS 13, iOS 16, tvOS 16, watchOS 8, *)
 public typealias NavigationStack = SwiftUI.NavigationStack
-
-
-public typealias Published = SwiftUI.Published
-public typealias StateObject = SwiftUI.StateObject
 
 #endif
 
