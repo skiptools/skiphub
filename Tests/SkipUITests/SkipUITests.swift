a31cbd0b3de4ec85c1d479fa8b91583399d9399d
marcprux <mwp1@cornell.edu>
2023-04-17 23:04:34 -0400

    Add Compose testing harness

--- Tests/SkipUITests/SkipUITests.swift
+++ Tests/SkipUITests/SkipUITests.swift
@@ -3,21 +3,45 @@
 // This is free software: you can redistribute and/or modify it
 // under the terms of the GNU Lesser General Public License 3.0
 // as published by the Free Software Foundation https://fsf.org
-#if !SKIP
 @testable import SkipUI
-#endif
 import XCTest
 import SkipFoundation
 
+// SKIP INSERT: import org.junit.Rule
+// SKIP INSERT: import androidx.compose.material.Text
+// SKIP INSERT: import androidx.compose.material.Button
+// SKIP INSERT: import androidx.compose.ui.Modifier
+// SKIP INSERT: import androidx.compose.ui.test.assertIsDisplayed
+// SKIP INSERT: import androidx.compose.ui.test.onNodeWithText
+// SKIP INSERT: import androidx.compose.ui.test.junit4.createComposeRule
+// SKIP INSERT: import androidx.test.ext.junit.runners.AndroidJUnit4
+// SKIP INSERT: import org.junit.runner.RunWith
+
+// SKIP INSERT: @RunWith(AndroidJUnit4::class)
+// SKIP INSERT: @org.robolectric.annotation.Config(manifest=org.robolectric.annotation.Config.NONE, sdk = [android.os.Build.VERSION_CODES.S])
 final class SkipUITests: XCTestCase {
+    // SKIP INSERT: @get:Rule val rule = createComposeRule()
+
     func testSkipUI() throws {
         XCTAssertEqual(3, 1 + 2)
         XCTAssertEqual("SkipUI", SkipUIInternalModuleName())
         XCTAssertEqual("SkipUI", SkipUIPublicModuleName())
-        //XCTAssertEqual("SkipFoundation", SkipFoundationPublicModuleName())
+        XCTAssertEqual("SkipFoundation", SkipFoundationPublicModuleName())
     }
 
-    // TODO:
-    //@get:Rule
-    //val rule = createComposeRule()
+
+    func testCompose() {
+        // SKIP INSERT: // DEMO
+        XCTAssertEqual("X", "X")
+        // SKIP INSERT: rule.setContent {
+        // SKIP INSERT:     Text(text = "Hello")
+        // SKIP INSERT: }
+        XCTAssertEqual("X", "X")
+    }
+
+    // SKIP INSERT: @Test func testComposeFunctions() {
+    // SKIP INSERT: rule.setContent {
+    // SKIP INSERT:     Text("Hello")
+    // SKIP INSERT: }
+    // SKIP INSERT: }
 }
