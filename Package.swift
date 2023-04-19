5402e58071b20da54c5dc1a673202383af74e81d
marcprux <mwp1@cornell.edu>
2023-04-19 09:00:13 -0400

    Update CI to only run against a single configuration

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         .library(name: "ExampleAppKt", targets: ["ExampleAppKt"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/skiptools/skip", from: "0.3.44"),
+        .package(url: "https://github.com/skiptools/skip", from: "0.3.46"),
     ],
     targets: [
         .target(name: "SkipUnit", dependencies: [.product(name: "SkipDriver", package: "skip")]),
