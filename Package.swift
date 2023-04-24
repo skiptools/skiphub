a4f903c363a8733e8be02b4709423a11b5a98ed2
marcprux <mwp1@cornell.edu>
2023-04-23 20:03:45 -0400

    CI: more resilient parallel testing

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         .library(name: "ExampleAppKt", targets: ["ExampleAppKt"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/skiptools/skip", from: "0.3.52"),
+        .package(url: "https://github.com/skiptools/skip", from: "0.3.53"),
     ],
     targets: [
         // Unit testing support: XCTest to JUnit conversion, Gradle test launch and results handling
