a8eea538e68d1e03acf5e4ade4b7e61abc82436d
marcprux <mwp1@cornell.edu>
2023-05-13 12:30:22 -0400

    Add appcompat to generated build.gradle.kts

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         .library(name: "ExampleAppKt", targets: ["ExampleAppKt"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/skiptools/skip", from: "0.4.31"),
+        .package(url: "https://github.com/skiptools/skip", from: "0.4.32"),
     ],
     targets: [
         // Unit testing support: XCTest to JUnit conversion, Gradle test launch and results handling
