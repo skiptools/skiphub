1ab01bb1f8f3cdfa68a51d9ae11f6103bcac2d18
marcprux <mwp1@cornell.edu>
2023-05-17 15:33:11 -0400

    Fix test imports

--- Tests/SkipFoundationTests/UserDefaultsTests.swift
+++ Tests/SkipFoundationTests/UserDefaultsTests.swift
@@ -3,9 +3,6 @@
 // This is free software: you can redistribute and/or modify it
 // under the terms of the GNU Lesser General Public License 3.0
 // as published by the Free Software Foundation https://fsf.org
-#if !SKIP
-@testable import SkipKit
-#endif
 import Foundation
 import XCTest
 
