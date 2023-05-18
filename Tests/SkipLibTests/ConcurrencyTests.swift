1a55a465d59201d4d324a71fbe26a749e7a3ca10
Abe White <abewhite@mac.com>
2023-05-18 14:56:32 -0500

    Tweak concurrency tests

--- Tests/SkipLibTests/ConcurrencyTests.swift
+++ Tests/SkipLibTests/ConcurrencyTests.swift
@@ -7,7 +7,6 @@ import XCTest
 // SKIP INSERT: import kotlinx.coroutines.*
 // SKIP INSERT: import kotlinx.coroutines.test.*
 
-//~~~
 final class ConcurrencyTests: XCTestCase {
     /* SKIP INSERT:
      @Test fun testSimpleValue() {
@@ -30,13 +29,15 @@ final class ConcurrencyTests: XCTestCase {
             return await asyncInt()
         }
         let task2 = Task.detached {
-            // SKIP REPLACE: this@ConcurrencyTests.asyncInt2()
             return await self.asyncInt2()
         }
         let value1 = await task1.value
         let value2 = await task2.value
         XCTAssertEqual(value1, 100)
         XCTAssertEqual(value2, 200)
+
+        let value3 = await asyncInt()
+        XCTAssertEqual(value3, 100)
     }
 
     func asyncInt() async -> Int {
